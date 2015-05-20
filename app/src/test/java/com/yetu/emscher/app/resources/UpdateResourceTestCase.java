cc51e2d9fefb5c9e0396d70190c79bca4f9dac7b
Till Klocke <till.klocke@yetu.com>
2015-05-20 10:04:11 +0200

    Added more tests for the update resource

--- app/src/test/java/com/yetu/emscher/app/resources/UpdateResourceTestCase.java
+++ app/src/test/java/com/yetu/emscher/app/resources/UpdateResourceTestCase.java
@@ -1,10 +1,15 @@
 package com.yetu.emscher.app.resources;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.yetu.emscher.app.UpdateRepository;
 import com.yetu.emscher.fakerepo.FakeUpdateRepo;
@@ -26,14 +31,24 @@ public class UpdateResourceTestCase {
 		}
 	}
 
+	private UpdateResource resource;
+
+	@Before
+	public void setup() {
+		ObjectGraph graph = ObjectGraph.create(new TestCaseModule());
+		resource = graph.get(UpdateResource.class);
+	}
+
+	private Request readRequest(String filename) throws JsonParseException,
+			JsonMappingException, IOException {
+		XmlMapper mapper = new XmlMapper();
+		Request request = mapper.readValue(new File(filename), Request.class);
+		return request;
+	}
+
 	@Test
 	public void testPingResponse() throws Exception {
-		ObjectGraph graph = ObjectGraph.create(new TestCaseModule());
-		UpdateResource resource = graph.get(UpdateResource.class);
-
-		XmlMapper mapper = new XmlMapper();
-		Request request = mapper.readValue(new File(
-				"src/test/resources/update-ping.request"), Request.class);
+		Request request = readRequest("src/test/resources/update-ping.request");
 
 		Response response = resource.call(request);
 		assertNotNull(response);
@@ -42,6 +57,19 @@ public class UpdateResourceTestCase {
 		assertNotNull(response.getApp().getPing());
 		assertNotNull(response.getApp().getEvent());
 		assertNull(response.getApp().getUpdatecheck());
+	}
+
+	@Test
+	public void testUpdateCheckRequest() throws Exception {
+		Request request = readRequest("src/test/resources/updatecheck.request");
+
+		Response response = resource.call(request);
+
+		assertNotNull(response);
+		assertNotNull(response.getDaystart());
+		assertNotNull(response.getApp());
+		assertNotNull(response.getApp().getPing());
+		assertNotNull(response.getApp().getUpdatecheck());
 	}
 
 }
